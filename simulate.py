2f9ce0ef20e3fd5f3a7d7d7dba2e45e9ebd0c7a5
QIU Ning <pkuqiuning@gmail.com>
2015-06-12 09:09:49 -0400

    add weight based order to simulate.py

--- simulate.py
+++ simulate.py
@@ -1,5 +1,63 @@
 #!/usr/bin/env python2.7
-'''simulate.py
+'''market simulator
+
+
+Data Structure
+---------------
+
+trades is a DataFrame [T, symbol, weight, price]:
+interp. one trade record per line
+
+T   symbol  weight  price
+..  AAPL    0.1     20.75
+...
+
+
+investment is a float [0, +inf)
+
+
+commission_rate is a float [0, +inf)
+    
+
+stockpos is a DataFrame [T symbol1 symbol2 ...]
+interp. the time series of amount of stock holding in portfolio
+
+T AAPL GOOG ...
+.. 2    -1  ...
+
+
+cashpos is a Series[T float]
+interp. the time series of cash left in pocket
+
+T
+.. 100001.32
+...
+
+
+costs is a Series[T float]
+interp. the time series of cumulative commissions of trading
+T
+.. 2.34
+...
+
+
+portfolio is composite of
+    - cash is float
+    - stocks is dict(symbol -> share amount)
+         
+
+order is a DataFrame
+interp. the time sorted orders to buy/sell some shares of stocks
+
+T   symbol      amount
+..  AAPL        4.2
+
+weight based order is a DataFrame
+interp. buy/sell stocks to specified target percentage of portfolio value
+
+T   symbol      percent
+..  GOOG        0.05
+
 '''
 
 
@@ -23,7 +81,7 @@ INVEST = 1e6
 PRICES_FILE = 'data/sp500.h5'
 ORDER_FILE = 'data/orders.txt'
 RESULTS_FILE = 'data/simulate_results.txt'
-FIGURE_FILE = 'data/simulate.png'
+FIGURE_FILE = 'pic/simulate.png'
 COMMITION_RATE = 0.001
 LOG_DIR = 'log'
 
@@ -48,48 +106,6 @@ def load_trades(trade_file, timezone='UTC'):
     '''load trade file from csv'''
     trades = pd.read_csv(trade_file, parse_dates=True, index_col=0)
     trades.index = trades.index.tz_localize(timezone).tz_convert('UTC')
-    return trades
-
-
-def do_transaction(t, symbol, amount, prices):
-    '''timestamp, str, float, dataframe -> transaction
-
-    return the transaction for given order, None for no transaction
-
-    '''
-    t0 = t.replace(second=0)
-    #tt = t0 if t0 in self._trading_time_index else None
-    #if tt:
-    #if t0:
-        #price = self.prices.loc[tt, symbol]
-    try:
-        price = prices.at[t0, symbol]
-        if np.isnan(price):
-            return None
-        #return (tt, symbol, amount, price)
-        return (t0, symbol, amount, price)
-    except KeyError:
-        return None
-
-
-def get_trades(orders, price_minute):
-    '''given trade orders, query price data to produce trades'''
-    transactions = []
-    #trading_time_index = price_minute.index
-
-    for i, row in enumerate(orders.iterrows()):
-        if i % 10000 == 0:
-            print >>sys.stderr, 'trading...', i, '\r',
-        t, v = row
-        # t: time
-        # v: [symbol, amount]
-        # query trading price
-        transaction = do_transaction(t, v['symbol'], v['amount'], price_minute)
-        if transaction:
-            #print transaction[0]
-            transactions.append(transaction)
-    trades = pd.DataFrame(transactions, columns=['T', 'symbol', 'amount', 'price'])
-    trades.set_index('T', inplace=True)
     return trades
 
 
@@ -125,6 +141,45 @@ def get_trades_fast(orders, price_minute):
     return trades
 
 
+def market_simulate_weight_based(trades_wt, price_minute, investment, commission_rate):
+    '''trades_wt, price_daily, investment, commission -> trades
+
+    excute market trades, produce transactions
+
+    Precondition:
+    trades should be time-ordered
+
+    
+    '''
+
+    cash = investment
+    stocks = np.zeros_like(price_minute.iloc[0])
+    symbol_numbers = price_minute.columns.values.searchsorted(trades_wt['symbol'])
+    records = []
+    for i, row_sn in enumerate(zip(trades_wt.iterrows(), symbol_numbers)):
+        if i % 1000 == 0:
+            print >>sys.stderr, i, '\r',
+        row, sn = row_sn
+        t, v = row
+        s, w, p = v
+        portfolio_value = (stocks * price_minute.loc[t]).sum() + cash
+        amount = w * portfolio_value / p
+        d_amount = amount - stocks[sn]
+
+        # update portfolio
+        stocks[sn] = amount
+        cash = cash - d_amount * p * (1 + commission_rate)
+
+        # record result
+        record = (t, s, d_amount, p)
+        records.append(record)
+        #print record
+    trades = pd.DataFrame.from_records(records)
+    trades.columns = ['T', 'symbol', 'amount', 'price']
+    trades.set_index('T', inplace=True)
+    return trades
+
+
 def get_trans(trades, rate, inplace=True):
     '''given trades and commission rate, produce transaction'''
     trans = trades if inplace else trades[:]
@@ -136,15 +191,16 @@ def get_trans(trades, rate, inplace=True):
 # EOD positions
 def get_trans_eod(trans):
     '''given excuted transactions, produce EOD transactions'''
-    trans_eod = trans.groupby('symbol').resample('D', how={'amount': sum, 'cashvalue': sum, 'commission': sum})
+    d_pos = trans.groupby('symbol').resample('D', how={'amount': sum, 'cashvalue': sum, 'commission': sum})
+    trans_eod = d_pos.unstack('symbol').dropna(how='all')
     return trans_eod
 
 
-def get_pos_eod(trans_eod, init=INVEST):
-    '''given EOD transactions, produce EOD stock and cash positions with total commissions'''
-    d_pos = trans_eod.unstack('symbol').dropna(how='all')
-    d_pos[d_pos.isnull()] = 0
-    pos = d_pos.cumsum()
+def get_pos_eod_daily(trans_eod, price_daily, init=INVEST):
+    '''given EOD transactions, produce EOD stock and cash positions with total commissions, use price data as shape'''
+    trans_eod.reindex(price_daily.index)
+    trans_eod[trans_eod.isnull()] = 0
+    pos = trans_eod.cumsum()
     stockpos = pos.amount
     cashpos = init - (pos.cashvalue + pos.commission).sum(axis=1)
     costs = pos.commission.sum(axis=1)
@@ -175,6 +231,7 @@ def main():
         description='Market simulator, provide order file or trade file, simulate market performance.',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
+    argp.add_argument('-wt', '--weight-based', action='store_true', help='order file will contain target weight')
     argp.add_argument('-i', '--investment', type=float, default=INVEST, help='initial investment')
     argp.add_argument('-c', '--commission', type=float, default=COMMITION_RATE, help='commition rate')
     argp.add_argument('-pf', '--price-file', default=PRICES_FILE, help='price file in hdf5 format')
@@ -188,7 +245,7 @@ def main():
 
     argp.add_argument('-nos', '--no-show', action='store_true', help='do not show figure')
     argp.add_argument('-log', '--log', action='store_true', help='output detailed information')
-    argp.add_argument('-it', '--interactive', action='store_true', help='enter interactive mode after running')
+    argp.add_argument('-debug', action='store_true', help='enter interactive mode after running')
     args = argp.parse_args()
 
 
@@ -222,22 +279,27 @@ def main():
         trades = load_trades(args.trade_file, timezone=args.time_zone)
         logging.debug('read {} trades'.format(len(trades)))
 
+    if args.weight_based:
+        logging.info('calculation running on weight based mode')
+        trades = market_simulate_weight_based(trades, price_minute, args.investment, args.commission)
+        if args.log:
+            trades.to_csv(LOG_DIR + 'trades_wt.log')
     trans = get_trans(trades, args.commission)
-
-    # EOD processes begin
-    logging.info('begin EOD calculation...')
     trans_eod = get_trans_eod(trans)
-    stockpos, cashpos, costs = get_pos_eod(trans_eod)
+    logging.info('begin EOD calculation...')
+    stockpos, cashpos, costs = get_pos_eod_daily(trans_eod, price_daily, args.investment)
     logging.debug('get {} EOD positions'.format(len(stockpos)))
+
     if args.log:
         stockpos.to_csv(LOG_DIR + 'stockpos.log')
         cashpos.to_csv(LOG_DIR + 'cashpos.log')
         costs.to_csv(LOG_DIR + 'costs.log')
+
     stock_values = get_stock_value(stockpos, price_daily)
     stock_total_values = get_stock_total_values(stock_values)
     values = get_values(stock_total_values, cashpos)
     logging.info('calculation EOD finished')
-    if args.interactive:
+    if args.debug:
         import interact
         interact.run(local=dict(locals(), **globals()))
 
@@ -255,18 +317,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-#def test():
-    #import doctest
-    #doctest.testmod()
-    ##sig = pd.read_hdf('data/sp500-smaller-signal.h5', '/signal')['2012-9':]
-    ##sample_trades = sig.iloc[:, 0:3]
-    ##sample_trades.columns = ['symbol', 'amount', 'price']
-    ##sample_trades.dropna(inplace=True)
-    ##sample_trades['tot'] = sample_trades.amount * sample_trades.price
-    ###sample_trades = pd.read_csv(trade_csv, index_col=0, parse_dates=True)
-    ##tm = sample_trades.groupby('symbol').resample('D', how='sum')
-    ###print sample_trades.price * sample_trades.amount
-    ##tm.unstack('symbol')
-
