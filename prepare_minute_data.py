a93d7e08b6847cab234388eb73f5903ba16754fb
Accern Dev <ahmed@accern.com>
2015-06-15 15:42:07 -0400

    arrange data

--- prepare_minute_data.py
+++ prepare_minute_data.py
@@ -7,24 +7,29 @@ __author__ = 'fantasy <pkuqiuning@gmail.com>'
 __created__ = '2015-05-26 14:59:39 -0400'
 
 
+import sys
 from backtest import load_bars_from_file
 import pandas as pd
 
 
 def main():
-    START = '2012-8-15'
-    END = '2015-2-19'
-    stocks = open('data/sp500.csv').read().splitlines()
+    START = '2010-1-1'
+    END = '2015-5-1'
+    stocks = open('data/sp500.txt').read().splitlines()
+    nstocks = len(stocks)
 
     prices = {}
     for i, stock in enumerate(stocks):
+        print >>sys.stderr, '\r {}/{} '.format(i, nstocks),
         df = load_bars_from_file(stock, start=START, end=END)
         close = df['close']
         prices[stock] = close
 
     prices_df = pd.DataFrame(prices)
-    prices_df.to_csv('sp500_close.csv')
-    #prices_df.to_hdf('data/sp500_close.h5')
+    print 'output to csv...'
+    prices_df.to_csv('sp500_close.csv.gz')
+    print 'output to hdf...'
+    prices_df.to_hdf('data/sp500_close.h5', 'minute/close)
 
 
 if __name__ == "__main__":
